124fdc344eba16ab00237deddd793a4bf39d4c73
Simone Esposito <31246543+DarkJoker360@users.noreply.github.com>
2018-07-16 11:36:05 +0200

    init cleaning

--- init/Android.mk
+++ init/Android.mk
@@ -6,15 +6,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
-<<<<<<< HEAD
 LOCAL_SRC_FILES := init_ms013g.cpp
 LOCAL_MODULE := libinit_ms013g
-=======
-LOCAL_SRC_FILES := init_s3ve3g.cpp
-LOCAL_MODULE := libinit_s3ve3g
 LOCAL_STATIC_LIBRARIES := libbase
 
->>>>>>> a30abcd... los15.1 bringup
 include $(BUILD_STATIC_LIBRARY)
 
 endif
